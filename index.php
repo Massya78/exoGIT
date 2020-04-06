6882f73c49f431d3e69498129780131aa5124de3
Massya KONE <massya.kone@gmail.com>
2020-04-06 14:35:39 +0200

    premiere version

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Exo</title>
+</head>
+<body>
+<div class="container">
+
+<h1 class="col-md-8 mx-auto display-4 text-center alert alert-success"></h1>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis 
+quibusdam delectus inventore sed atque harum ex, ea aut accusantium quas 
+repudiandae quos deserunt assumenda non asperiores unde labore voluptatibus rem.</p>
+
+</div>   
+</body>
+</html>
